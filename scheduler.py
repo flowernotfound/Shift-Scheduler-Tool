16fe982eb984fc2852d8731eea7d847ce3150639
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-11-03 20:37:26 +0900

    created schedular_test

--- scheduler.py
+++ scheduler.py
@@ -5,15 +5,15 @@ import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
 
 # CSVファイルのパス
-file_path = './input/test.csv'
+file_path = ''
 
-# CSVファイルの読み込み
-try:
-    # Attempt to read the CSV with default encoding
-    shift_preferences = pd.read_csv(file_path)
-except UnicodeDecodeError:
-    # If there's an encoding error, try a common alternative encoding
-    shift_preferences = pd.read_csv(file_path, encoding='utf-8-sig')
+# # CSVファイルの読み込み
+# try:
+#     # Attempt to read the CSV with default encoding
+#     shift_preferences = pd.read_csv(file_path)
+# except UnicodeDecodeError:
+#     # If there's an encoding error, try a common alternative encoding
+#     shift_preferences = pd.read_csv(file_path, encoding='utf-8-sig')
 
 # 特定の日に対してシフト希望を抽出し、早番と遅番を割り当てる関数
 def assign_shifts_for_day(preferences, day):
@@ -64,15 +64,42 @@ def create_shift_schedule(preferences):
 # GUI用
 def select_file():
     # ファイル選択ダイアログを開いてファイルパスを取得する関数
-    pass
+    global file_path, generate_button
+    file_path = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
+    if file_path:
+        file_label.config(text=file_path)
+        generate_button.config(state='normal')
+    else:
+        file_label.config(text="ファイルが選択されていません")
+        generate_button.config(state='disabled')
 
 def display_shift_schedule():
-    # シフトスケジュールを生成して表示する関数
-    pass
+    # グローバル変数を宣言
+    global file_path, shift_schedule
+    try:
+        shift_preferences = pd.read_csv(file_path)
+        shift_schedule = create_shift_schedule(shift_preferences)  # この変数をグローバルに設定
+        # 結果をテキストエリアに表示
+        result_text.delete('1.0', tk.END)
+        result_text.insert(tk.END, shift_schedule.to_string())
+        export_button.config(state='normal')
+    except Exception as e:
+        messagebox.showerror("エラー", str(e))
+        export_button.config(state='disabled')
+
 
 def export_shift_schedule():
-    # シフトスケジュールをCSVとしてエクスポートする関数
-    pass
+    # グローバル変数を宣言
+    global file_path, shift_schedule
+    # exportの処理はここで完結するようにします
+    if shift_schedule is not None:
+        output_path = filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("CSV files", "*.csv")])
+        if output_path:
+            try:
+                shift_schedule.to_csv(output_path, index=False)
+                messagebox.showinfo("成功", "シフトスケジュールがエクスポートされました。")
+            except Exception as e:
+                messagebox.showerror("エラー", str(e))
 
 # ここからGUIの起動コード
 root = tk.Tk()
@@ -101,12 +128,12 @@ export_button.pack()
 # メインループ
 root.mainloop()
 
-# シフトスケジュールを作成
-shift_schedule = create_shift_schedule(shift_preferences)
+# # シフトスケジュールを作成
+# shift_schedule = create_shift_schedule(shift_preferences)
 
-# CSVファイルとしてシフトスケジュールを保存する
-output_path = './output/shift_schedule.csv'
-shift_schedule.to_csv(output_path)
+# # CSVファイルとしてシフトスケジュールを保存する
+# output_path = './output/shift_schedule.csv'
+# shift_schedule.to_csv(output_path)
 
-# 結果を確認
-shift_schedule.head()
+# # 結果を確認
+# shift_schedule.head()
